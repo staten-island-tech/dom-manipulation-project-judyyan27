c4a3b4262f30e8d45e612caa23823792efc7e02f
Judy <158211720+judyyan27@users.noreply.github.com>
2024-10-15 12:19:57 -0400

    help :sob:

--- index.js
+++ index.js
@@ -1,19 +1,15 @@
-console.log("connected");
-
 const DOMSelectors = {
-  header: document.querySelectorbyid("#header"),
-  text: document.querySelector(".text"),
-  items: document.querySelectorAll("list"),
-  button: document.querySelectorAll("btn"),
+  header: document.querySelector("h2"),
+  input_desc: document.querySelector(".desc"),
+  form: document.querySelector("form"),
+  button: document.querySelectorbyid("#btn"),
 };
 
-console.log(DOMSelectors.header);
-console.log(DOMSelectors.text);
+console.log(DOMSelectors.input_desc);
 
 function changeColor() {
-  let button = document.querySelector("btn");
-  console.log(button);
-  button.addEventListener("click", function (event) {
+  let button = DOMSelectors.button;
+  button.button.addEventListener("click", function () {
     // listen to click even
     event.preventDefault();
     // prevents default behavior
@@ -24,14 +20,15 @@ function changeColor() {
 }
 changeColor();
 
-//DOMSelectors.form.addEventListener("submit", function (event)){
+// DOMSelectors.form.addEventListener("submit", function ()){
 
-//select ALL list items
-const item = document.querySelectorAll("li");
-//turn the nodelist into an Array
-const items = Array.from(item);
-//iterate/Loop over array and for each element make the color red
-items.forEach((el) => (el.style.color = "red"));
+
+
+
+DOMSelectors.form.addEventListener("submit", function (event) {
+  event.preventDefault();
+  console.lof(document.querySelector("input").value);
+});
 
 const buttons = document.querySelectorAll("button");
 buttons.forEach((btn) =>
@@ -41,11 +38,19 @@ buttons.forEach((btn) =>
 );
 let card = {
   name: "Name here",
+  
 };
 
-DOMSelectors.container.insertAdjacentHTML("beforeend");
+DOMSelectors.container.insertAdjacentHTML("beforeend"); */
 
 // event listener for form
 // get values from form for widget object
 // create card, insert the card
 // find remove buttons and add event listeners
+
+//select ALL list items
+const item = document.querySelectorAll("li");
+//turn the nodelist into an Array
+const items = Array.from(item);
+//iterate/Loop over array and for each element make the color red
+items.forEach((el) => (el.style.color = "red")); 
