c18d0fed6fc3fbc1705af71b5bdab6bb0b88008e
Judy <158211720+judyyan27@users.noreply.github.com>
2024-10-18 11:25:05 -0400

    e

--- index.js
+++ index.js
@@ -1,12 +1,30 @@
 const DOMSelectors = {
   header: document.querySelector("h2"),
-  input_desc: document.querySelector(".desc"),
   form: document.querySelector("form"),
   button: document.querySelectorbyid("#btn"),
+  box: document.querySelectorbyid("#container-box"),
+  name: document.querySelectorbyid("#name"),
+  description: document.querySelectorbyid("#description"),
 };
+button.addEventListener("click", function (event) {
+  event.preventDefault();
 
-console.log(DOMSelectors.input_desc);
+  let name = DOMSelectors.name.value;
+  let description = DOMSelectors.description.value;
 
+  console.log(document.querySelector("input").value);
+
+  DOMSelectors.box.insertAdjacentHTML(
+    "beforeend",
+    `<div id="container-box">
+    <div class="card">
+      <h1 class="card-title">${name}</h1>
+      <p class="text">${description}</p>
+    </div> 
+    </div>`
+  );
+});
+/* 
 function changeColor() {
   let button = DOMSelectors.button;
   button.button.addEventListener("click", function () {
@@ -22,9 +40,6 @@ changeColor();
 
 // DOMSelectors.form.addEventListener("submit", function ()){
 
-
-
-
 DOMSelectors.form.addEventListener("submit", function (event) {
   event.preventDefault();
   console.lof(document.querySelector("input").value);
@@ -36,21 +51,10 @@ buttons.forEach((btn) =>
     console.log(event.target);
   })
 );
-let card = {
-  name: "Name here",
-  
-};
 
-DOMSelectors.container.insertAdjacentHTML("beforeend"); */
+ */
 
 // event listener for form
 // get values from form for widget object
 // create card, insert the card
 // find remove buttons and add event listeners
-
-//select ALL list items
-const item = document.querySelectorAll("li");
-//turn the nodelist into an Array
-const items = Array.from(item);
-//iterate/Loop over array and for each element make the color red
-items.forEach((el) => (el.style.color = "red")); 
